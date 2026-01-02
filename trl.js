c7a52cc71aa89aa937c4fd9a6c7acd0cc6b4172c
mnino3677-jpg <mnino3677@gmail.com>
2026-01-02 21:03:23 +0100

    Add files via upload

---
+++ trl.js
@@ -0,0 +1,2 @@
+// trl.js
+const webhookUrl = 'https://discord.com/api/webhooks/1454865487484944466/weY8KRVP7QElct3yQ_2lGU8U4HArlH4ZglyEjEy5_tH733evL3sM2AyTSVdiBpuQvRQ9'; // Reemplaza esto con tu Webhook real
